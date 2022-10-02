b406f0c6263abc6046b532fae05b268e5289b364
saurabh-bot0909 <114792204+saurabh-bot0909@users.noreply.github.com>
2022-10-02 15:36:55 +0530

    Binary search in java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,30 +1,35 @@
-class BinarySearch {
 
-    public static void main(String args[]) {
-        BinarySearch ob = new BinarySearch();
 
-        int arr[] = { 12,43,46,76,98,26,6,83 };
+// Binary Search java code
+
+public class BinarySearch
+{
+	public static void main(String[] args) {
+		int arr[] = {3, 6, 7, 11, 20, 28, 35, 100};
+		int x = 28;
+		int position = binarySearch(arr, x);
+		if(position == -1)
+		    System.out.println(x + " is not present in the given array");
+		else
+		    System.out.println(x + " is present at " + position + " in the given array");
+	}
+	
+	// searching a number in a given sorted array
+	static int binarySearch(int[] arr, int x)
+    {
+        int l = 0, r = arr.length-1;
         
-        int n = arr.length;
-
-        int x = 43;
-
-        int result = ob.binarySearch(arr, 0, n - 1, x);
-        if (result == -1)
-            System.out.println("Element not present");
-        else
-            System.out.println("Element found at index " + result);
-    }
-
-    int binarySearch(int arr[], int l, int r, int x) {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] == x)
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+  
+            if (arr[mid] == x)     // if x is present at mid
                 return mid;
-            if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-            return binarySearch(arr, mid + 1, r, x);
+  
+            if (arr[mid] < x)      // if x greater, ignore left half
+                l = mid + 1;
+            else                 // if x is smaller, ignore right half
+                r = mid - 1;
         }
-        return -1;
+        return -1;               // if x is not present in array
     }
 }

c31c13769f389a1ec67755118612a817ecdec915
netkap <100796046+netkap@users.noreply.github.com>
2022-10-03 22:52:51 +0530

    Create palindromeCheck.cpp

---
+++ palindromeCheck.cpp
@@ -0,0 +1,16 @@
+int isPalindrome(string S)
+{
+    int start = 0, end = S.length() - 1;
+
+    while (start < end)
+    {
+        if (S[start] != S[end])
+        {
+            return 0;
+        }
+        start++;
+        end--;
+    }
+
+    return 1;
+}
